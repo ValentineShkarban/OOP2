9a6499dcdc7629d6ad2a496c6b6561d4e71f3201
Valentine <vals91@outlook.com>
2024-10-21 16:28:57 +0300

    maxStations

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -384,4 +384,15 @@ class RadioTest {
         int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testMaxStations() {
+        Radio radio = new Radio(20);
+
+        radio.setCurrentStation(15);
+
+        int expected = 15;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
 }
