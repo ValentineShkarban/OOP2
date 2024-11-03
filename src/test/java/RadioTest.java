a2ae57e43dc13bae4fe7f8055ca7351940517501
ValentineShkarban <vals91@outlook.com>
2024-11-04 02:33:37 +0300

    Update RadioTest.java

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -384,15 +384,4 @@ class RadioTest {
         int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    public void test() {
-        Radio radio = new Radio(20);
-
-        radio.setCurrentStation(15);
-
-        int expected = 15;
-        int actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
 }
